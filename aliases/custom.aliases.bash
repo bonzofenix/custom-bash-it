3f13945d340bec276282ddf1de1657cad02cb81a
Alan Moran & Leandro Cacciagioni <pair+bonzofenix+lcacciagioni@wmg.com>
2014-07-16 18:46:19 +0000

    add fuc$ openstack vagrant alias

--- aliases/custom.aliases.bash
+++ aliases/custom.aliases.bash
@@ -5,4 +5,5 @@ alias fuck_bosh='bosh -n create release --force && bosh -n upload release && bos
 alias fuck_bosh_recreate='bosh -n create release --force && bosh -n upload release && bosh -n deploy --recreate'
 alias fuck_virtualbox='sudo /Library/StartupItems/VirtualBox/VirtualBox restart'
 alias fuck_vagrant_aws='vagrant destroy -f && sleep 40 && vagrant up --provider=aws; beep'
+alias fuck_vagrant_openstack='vagrant destroy -f && sleep 5 && vagrant up --provider=openstack; beep'
 alias fuck_vagrant='vagrant destroy -f && vagrant up ; beep'
