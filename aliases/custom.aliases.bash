7e111f511481a6bf2103fa370e54c3e6042ef628
Alan Moran <bonzofenix@gmail.com>
2014-07-16 11:44:15 -0700

    be for bundle exec alias

--- aliases/custom.aliases.bash
+++ aliases/custom.aliases.bash
@@ -6,3 +6,9 @@ alias fuck_bosh_recreate='bosh -n create release --force && bosh -n upload relea
 alias fuck_virtualbox='sudo /Library/StartupItems/VirtualBox/VirtualBox restart'
 alias fuck_vagrant_aws='vagrant destroy -f && sleep 40 && vagrant up --provider=aws; beep'
 alias fuck_vagrant='vagrant destroy -f && vagrant up ; beep'
+alias be='bundle exec'
+
+if [ ! $(uname -s) = "Darwin" ]; then
+  alias pbcopy='xsel --clipboard --input'
+  alias pbpaste='xsel --clipboard --output'
+fi
--- lib/custom.bash
+++ lib/custom.bash
@@ -1,3 +1,4 @@
 # Custom bash
 
 export PATH="~/.bash_it/bin:$PATH"
+echo "gem: --no-ri --no-rdoc" > ~/.gemrc
